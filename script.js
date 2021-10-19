335b6ba58f4db00c26d6b8f9bff9b318be6f45a1
Maria Thais Ferreira Ramos <maria.ramos@mercadolivre.com>
2021-10-19 13:00:47 -0300

    premisas js

--- script.js
+++ script.js
@@ -9,21 +9,25 @@ let keyPressArray = [];
 
 document.onkeypress = (event) => {
     const onEventKeyPress = event.keyCode === 13 || event.key === 'Enter' ? confirmKeyPress(keyPressArray) : keyPressArray.push(event.key);
+    
     return onEventKeyPress;
 };
 
 const confirmKeyPress = (array) => {
-    const resultKeyPress = array.join('');
-    const filterCollaborator = collaborators.filter((item) => resultKeyPress === item.id);
-
-    const authorizationCheck = !filterCollaborator[0] ? successOrFailed('failed', 'Collaborator not found, contact the administrative department!')
-    : filterCollaborator[0].autorization ? responseCollaborator(filterCollaborator) 
-    : successOrFailed('failed', `${filterCollaborator[0].name} you are not allowed to enter this area`)
-
-    console.log('update', collaborators);
-
-    keyPressArray = [];
-    return authorizationCheck;
+    renderingSpinner();
+    setTimeout(() => {
+        const resultKeyPress = array.join('');
+        const filterCollaborator = collaborators.filter((item) => resultKeyPress === item.id);
+    
+        const authorizationCheck = !filterCollaborator[0] ? successOrFailed('failed', 'Collaborator not found, contact the administrative department!')
+        : filterCollaborator[0].autorization ? responseCollaborator(filterCollaborator) 
+        : successOrFailed('failed', `${filterCollaborator[0].name} you are not allowed to enter this area`)
+    
+        console.log('update', collaborators);
+    
+        keyPressArray = [];
+        return authorizationCheck;
+    }, 200)
 }
 
 // negotiation for authorized collaborator
@@ -36,23 +40,19 @@ const checkInorOut = async (collaborator, boolean) => {
     const mapColaborator = collaborator.map((col) => {
         Object.assign(col, {enter: boolean})
     })
-    await successOrFailed('success');
-    boolean ? alert(`Welcome ${collaborator[0].name}`) : alert(`See you later ${collaborator[0].name}`);
+    await 
+    boolean ? successOrFailed('success', `Welcome ${collaborator[0].name}`) 
+    : successOrFailed('success', `See you later ${collaborator[0].name}`)
     return mapColaborator;
 }
-
 console.log(keyPressArray);
-
-
-const successOrFailed = (classe, text) => {
-    renderingSpinner()
+const successOrFailed = (classe, text) => { 
     const imgI = document.querySelector('.bi-person-bounding-box');
     imgI.classList.add(classe);
     
-    setTimeout(() => imgI.classList.remove(classe), 2000);
+    setTimeout(() => imgI.classList.remove(classe), 1500);
     console.log(text);
 }
-
 
 const renderingSpinner = () => {
     const containerSpinner = document.querySelector('.spinner-container');
@@ -65,5 +65,5 @@ const renderingSpinner = () => {
     $(containerSpinner).append(html);
     setTimeout(() => {
         containerSpinner.innerText = '';
-    }, 50)
+    }, 210)
 }
